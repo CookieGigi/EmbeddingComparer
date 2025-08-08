89894e8565ebe531205ad8f64dd01f12cac1f050
cookiegigi <gcreusot@pm.me>
2025-08-08 13:53:05 +0800

    test: storybook setup to work with tailwind and shadcn

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+	stories: ["../src/**/*.stories.tsx", "../src/**/*.mdx"],
+	addons: [
+		"@chromatic-com/storybook",
+		"@storybook/addon-docs",
+		"@storybook/addon-onboarding",
+		"@storybook/addon-a11y",
+		"@storybook/addon-vitest",
+		"@storybook/addon-styling-webpack",
+		"@storybook/addon-themes",
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {},
+	},
+	core: {
+		disableTelemetry: true, // 👈 Disables telemetry
+	},
+};
+export default config;
